e87ed3044bc7ef27a231927d6d5ca0637e2e674f
Thalyson <61944196+nooneknowws@users.noreply.github.com>
2024-10-07 17:10:24 -0300

    tela de editar perfil WIP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CadastroTecnicoComponent } from './auth/cadastro-tecnico/cadastro-tecnico.component';
 import { LoginComponent } from './auth/login/login.component';
+import { EditarPerfilComponent } from './Cliente/perfil/editar-perfil/editar-perfil.component';
 
 export const routes: Routes = [
     { path: 'cadastro-tecnico', component: CadastroTecnicoComponent },
-    { path: 'login', component: LoginComponent }
-];
+    { path: 'login', component: LoginComponent },
+    { path: 'cliente/editar-perfil', component: EditarPerfilComponent}
+]
