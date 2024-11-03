aa456f894c9008fea4c1e698ce16533804330fab
Thalyson <61944196+nooneknowws@users.noreply.github.com>
2024-11-03 18:51:20 -0300

    refatoração do projeto

---
+++ backend/forms/src/test/java/br/ufpr/tcc/MSForms/FormsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpr.tcc.MSForms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
