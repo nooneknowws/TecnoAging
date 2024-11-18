1176cdc8092215278cdf4703100a9a5426cb0e2d
unknown <fumireko@yandex.com>
2024-11-17 22:25:30 -0300

    Dividir o componente de navbar para os perfis de técnico e paciente

--- frontend/src/app/tecnico/tecnico-layout/tecnico-layout.component.ts
+++ frontend/src/app/tecnico/tecnico-layout/tecnico-layout.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
+import { AuthService } from '../../_shared/services/auth.service';
 
 @Component({
   selector: 'app-tecnico-layout',
   templateUrl: './tecnico-layout.component.html',
-  styleUrl: './tecnico-layout.component.css'
+  styleUrls: ['./tecnico-layout.component.css']
 })
 export class TecnicoLayoutComponent {
   linkVoltarVisivel: boolean = false;
-  
-  constructor(private router: Router) {}
+  isNavbarOpen: boolean = true;
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.router.events.pipe(
@@ -20,11 +25,20 @@ export class TecnicoLayoutComponent {
     });
   }
 
+  toggleNavbar(): void {
+    this.isNavbarOpen = !this.isNavbarOpen;
+  }
+
   voltar() {
     history.back();
   }
 
   isRotaInicial(): boolean {
     return this.router.url === '/tecnico';
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
