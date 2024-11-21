de876d16449d5dc5c3eea3072fd15ad4ba6367c0
Gabriel Chioquetta <gabriel.obladen@gmail.com>
2024-11-21 19:44:33 -0300

    fix

--- frontend/src/app/tecnico/comparar-resultados/comparar-resultados.component.ts
+++ frontend/src/app/tecnico/comparar-resultados/comparar-resultados.component.ts
@@ -9,7 +9,7 @@ export class CompararResultadosComponent implements OnInit {
   testes = [
     {
       tipo: 'Nível de Atividade Física',
-      tempoTotal: '08:30',
+      dataFinalizacao: '20/11/2024 14:30',
       pontuacaoTotal: 85,
       detalhes: [
         { texto: 'Quanto tempo por dia você realiza atividades vigorosas?', resposta: '01:30', media: '00:45' },
@@ -18,7 +18,7 @@ export class CompararResultadosComponent implements OnInit {
     },
     {
       tipo: 'Índice de Vulnerabilidade',
-      tempoTotal: '06:20',
+      dataFinalizacao: '10/11/2024 16:50',
       pontuacaoTotal: 72,
       detalhes: [
         { texto: 'Em geral, comparado com outras pessoas da sua idade, como você diria que está a sua saúde?', resposta: 'Regular ou ruim', media: 'Regular ou ruim' },
@@ -27,7 +27,7 @@ export class CompararResultadosComponent implements OnInit {
     },
     {
       tipo: 'Mini Exame do Estado Mental',
-      tempoTotal: '07:15',
+      dataFinalizacao: '02/11/2024 09:30',
       pontuacaoTotal: 90,
       detalhes: [
         { texto: 'Que dia é hoje?', resposta: 'Correto', media: '80% Correto' },
