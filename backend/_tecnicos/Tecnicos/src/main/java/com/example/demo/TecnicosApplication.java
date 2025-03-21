e241c03e5eef6b7066d2a64129e6ac6e1004ef5a
Thalyson <thalysonbruck@hotmail.com>
2025-03-21 19:03:03 -0300

    Criação dos projetos e mudanças na estrutura do git

---
+++ backend/_tecnicos/Tecnicos/src/main/java/com/example/demo/TecnicosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnicosApplication.class, args);
+	}
+
+}
